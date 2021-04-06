cbfe84eefc9b715816c2ab3e909dd42d5ef7005f
puntart <thiagopuntar@gmail.com>
2021-04-06 15:46:42 -0300

    fix(realStateService): rm comments getUnitPrice

--- src/real-state/real-state.service.ts
+++ src/real-state/real-state.service.ts
@@ -17,17 +17,10 @@ export class RealStateService {
     return new RealStatePrice(price);
   }
 
-  async getUnitPrice(zipCode: string): Promise<any> {
-    try {
-      const url = `${process.env.BASE_PRICE_URL}?zipCode=${zipCode}`;
-      console.log(url);
-      const response = await this.httpService.get(url);
-      response.subscribe((x) => console.log(x.data));
-      return response;
-      // const { value } = response.data;
-      // return parseFloat(value);
-    } catch (error) {
-      console.log('error ', error.message);
-    }
+  async getUnitPrice(zipCode: string): Promise<number> {
+    const url = `${process.env.BASE_PRICE_URL}?zipCode=${zipCode}`;
+    const response = await this.httpService.get(url).toPromise();
+    const { value = 15 } = response.data;
+    return value;
   }
 }
